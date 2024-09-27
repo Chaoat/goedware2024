3a36d6cf5921bd7f1effa2a42274f939c6ef08b1
Putter <chantapatshea@gmail.com>
2024-09-27 14:17:48 +1000

    Got 3D movement working

---
+++ 3D_player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody3D
+
+@export var speed = 5.0
+@export var rotation_speed = 0.05
+
+func _physics_process(delta):
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var linear_dir = input_dir.y
+	var rot_dir = input_dir.x * -rotation_speed
+	print(rotation.y)
+	
+	if linear_dir:
+		velocity = basis.z.normalized() * speed * linear_dir
+		
+	else:
+		#velocity = Vector3.ZERO
+		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
+		
+	rotate_y(rot_dir)
+	
+	
+	move_and_slide()

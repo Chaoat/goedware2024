e407046662756c09631f12355aa13ef15776eb60
Chaoat <jstevadore@gmail.com>
2024-09-29 14:28:11 +1000

    Walk away from conversations

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,14 +1,16 @@
 extends Node
 
-var convincingness : float = 100
+static var maxConvincingness : int = 200
+
+var convincingness : float = maxConvincingness
 
 func _process(delta: float) -> void:
-	if convincingness > 100:
-		convincingness = 100
+	if convincingness > maxConvincingness:
+		convincingness = maxConvincingness
 
 func addConvincingness(amount:float):
 	convincingness = convincingness + amount
 	if convincingness < 0:
 		convincingness = 0
-	elif convincingness > 100:
-		convincingness = 100
+	elif convincingness > maxConvincingness:
+		convincingness = maxConvincingness
